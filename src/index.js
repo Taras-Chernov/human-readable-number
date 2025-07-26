95422222e0179c98702ab7de40c77fe996a5dab3
Taras <taras.chernov@mail.ru>
2025-07-26 18:21:28 +0300

    feat: fix lint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
-module.exports = function toReadable(/* number */) {
-  const toTwentyNumber = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const twoDigitNumbers = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    const hundred = ' hundred'
+module.exports = function toReadable(number) {
+  const toTwentyNumber = [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const twoDigitNumbers = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  const hundred = ' hundred';
 
-    let result = "";
-    if (number === 0) return "zero"
-    if (number <= 19) return toTwentyNumber[number]
-    if (number < 100) return twoDigitNumbers[Math.floor(number / 10)] + (number % 10 ? ' ' + toTwentyNumber[number % 10] : '');
-    return toTwentyNumber[Math.floor(number / 100)] + hundred + (number % 100 ? ' ' + toReadable(number % 100) : '');
-}
+  if (number === 0) return 'zero';
+  if (number <= 19) return toTwentyNumber[number];
+  if (number < 100)
+    return (
+      twoDigitNumbers[Math.floor(number / 10)] +
+      (number % 10 ? ` ${toTwentyNumber[number % 10]}` : '')
+    );
+  return (
+    toTwentyNumber[Math.floor(number / 100)] +
+    hundred +
+    (number % 100 ? ` ${toReadable(number % 100)}` : '')
+  );
+};
